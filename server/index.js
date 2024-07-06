9336ee930e164f11a0836e6beae96affb4e08b51
Yata Rupesh <99956608+RupeshRoyal@users.noreply.github.com>
2024-07-06 09:15:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ const express = require("express");
 const app = express();
 
 // const userRoutes = require("./routes/User");
-const profileRoutes = require("./routes/Profile");
-const paymentRoutes = require("./routes/Payments");
-const courseRoutes = require("./routes/Course");
-const contactUsRoute = require("./routes/Contact");
+// const profileRoutes = require("./routes/Profile");
+// const paymentRoutes = require("./routes/Payments");
+// const courseRoutes = require("./routes/Course");
+// const contactUsRoute = require("./routes/Contact");
 
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
